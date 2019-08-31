91a898d68b457b588f224fb4139334efa3daf1cf
Cuthbert20 <knowltonspencer@gmail.com>
2019-08-31 15:51:33 -0600

    working on comps in html & css

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Nav.css'
+
+export default function Nav() {
+    return (
+        <nav>
+            <button className='nav-btn' ><i class="fas fa-cocktail"></i></button>
+            
+        </nav>
+    )
+}
