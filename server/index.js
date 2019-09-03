fed4e6f697e194d4bcf81a8439d3ca8d98cdc87c
Cuthbert20 <knowltonspencer@gmail.com>
2019-09-03 16:41:13 -0600

    messy but, passed off comps

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const { CONNECTION_STRING, SERVER_PORT, SESSION_SECRET } = process.env
 const massive = require('massive')
 const session = require('express-session')
 const ctrl = require('./controller.js')
+const router = express.Router()
 
 //top level middlewear
 app.use(express.json())
@@ -16,13 +17,45 @@ app.use(session({
         maxAge: 1000 * 60 * 60 * 24
     }
 }))
+//middleware has access to the request obj, response obj,
+//and a function called next in the applications request-response cycle.
+//middeware funcs can perform the following tasks:
+//execute any code
+//make changes to the req and res objs
+//end the request-response cycle
+//call the next middleware in the stack
+//types 
+//app level middleware
+//router level middleware
+//error-handling middleware, error-handling middleware takes in 4 arguments you must provied four arguments
 
+let myMovie = (req,res,next) => {
+    console.log('Movies are cool')
+    next()
+}
+let getTime = (req,res,next) => {
+    req.getTime = Date.now()
+    next()
+}
+app.use(function (err, req, res, next) {
+    console.error(err.stack)
+    res.status(500).send('its broken')
+  })
+// app.use(getTime)
+app.use(myMovie)
 app.use("/static", express.static("./media"))
 app.use( express.static( `${__dirname}/../build` ) );
 
+//router level middleware
+// router.use(req,res,next){
+//     console.log("time:", new Date())
+//     next()
+// }
+
 //endpoints
-app.post('/auth/login', ctrl.login)
+app.post('/auth/login', getTime, ctrl.login)
 app.get('/api/movies', ctrl.movie)
+router.get("api/movies")
 // app.get()
 
 massive(CONNECTION_STRING).then(db => {
